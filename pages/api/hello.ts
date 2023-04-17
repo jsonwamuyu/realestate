83dbec1141e3fbe24b1c56d17c5c0e7be3f35b5f
jsonwamuyu <jysonmuchiri@gmail.com>
2023-04-17 13:56:26 -0700

    first commit

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -11,3 +11,20 @@ export default function handler(
 ) {
   res.status(200).json({ name: 'John Doe' })
 }
+// const axios = require("axios");
+
+// const options = {
+//   method: 'GET',
+//   url: 'https://bayut.p.rapidapi.com/auto-complete',
+//   params: {query: 'abu dhabi', hitsPerPage: '25', page: '0', lang: 'en'},
+//   headers: {
+//     'X-RapidAPI-Key': '1c6a99e45bmshff8436199eb00b0p1d6dd5jsn0798304bb861',
+//     'X-RapidAPI-Host': 'bayut.p.rapidapi.com'
+//   }
+// };
+
+// axios.request(options).then(function (response) {
+// 	console.log(response.data);
+// }).catch(function (error) {
+// 	console.error(error);
+// });
